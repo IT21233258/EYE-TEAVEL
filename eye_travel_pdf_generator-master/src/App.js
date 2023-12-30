820f007ec305f88a0f3bb0c2c2415d7e29b1e6ae
Tharindu Meegoda <99631031+IT21233258@users.noreply.github.com>
2023-12-30 22:43:25 +0530

    Add files via upload

---
+++ eye_travel_pdf_generator-master/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import PdfGenerator from './PdfGenerator';
+
+function App() {
+  return (
+    <PdfGenerator />
+  );
+}
+
+export default App;
